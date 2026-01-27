3f878680e8ff3806ea7e1d0b039290599f60aae7
Maxprime2.0 <maxence.ribautepicard@edu.ece.fr>
2026-01-27 17:10:46 +0100

    Correction cours 2

--- ExoCours2/lib/main.dart
+++ ExoCours2/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:formation_flutter/l10n/app_localizations.dart';
 import 'package:formation_flutter/res/app_colors.dart';
-import 'package:formation_flutter/screens/home_page.dart';
-
+//import 'package:formation_flutter/screens/home_page.dart';
+import 'package:formation_flutter/screens/product_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         textTheme: TextTheme(headlineMedium: TextStyle()),
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const ProductPage(),
     );
   }
 }
--- ExoCours2/lib/screens/product_page.dart
+++ ExoCours2/lib/screens/product_page.dart
@@ -1,108 +1,238 @@
 import 'package:flutter/material.dart';
+import 'package:formation_flutter/l10n/app_localizations.dart';
+import 'package:formation_flutter/model/product.dart';
+import 'package:formation_flutter/res/app_colors.dart';
+import 'package:formation_flutter/res/app_icons.dart';
+import 'package:formation_flutter/res/app_theme_extension.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
+  static const double IMAGE_HEIGHT = 300.0;
+
   @override
   Widget build(BuildContext context) {
-     return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Image.asset(
-          'assets/photo_entete.jpg',
-          height: 1000,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ), 
-        
-        Container(
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),   
-              topRight: Radius.circular(16),  
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            PositionedDirectional(
+              top: 0.0,
+              start: 0.0,
+              end: 0.0,
+              height: IMAGE_HEIGHT,
+              child: Image.network(
+                'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=1310&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                fit: BoxFit.cover,
+                cacheHeight:
+                    (IMAGE_HEIGHT * MediaQuery.devicePixelRatioOf(context))
+                        .toInt(),
+              ),
             ),
-          ),
-        
-          padding: const EdgeInsets.only(
-            top: 30,      
-            left: 20,    
-            right: 20,
-            bottom: 20,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Avocado Toast',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const Text(
-                'Grand Restaurant de petit dej',
-                style: TextStyle(color: Colors.grey, fontSize: 16),
-              ),
-            Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                const InfoLigne(
-                  libelle: 'Format',
-                  valeur: '200g',
+            PositionedDirectional(
+              top: IMAGE_HEIGHT - 16.0,
+              start: 0.0,
+              end: 0.0,
+              bottom: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(16.0),
+                  ),
+                  color: Colors.white,
                 ),
-                
-                const InfoLigne(
-                  libelle: 'Orgine',
-                  valeur: 'France',
-                  showDivider: false, // On cache le dernier séparateur
+                padding: EdgeInsetsDirectional.symmetric(
+                  horizontal: 20.0,
+                  vertical: 30.0,
                 ),
-                ],
-          ),
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    Text(
+                      'Petits pois et carottes',
+                      style: context.theme.title1,
+                    ),
+                    Text('Cassegrain', style: context.theme.title2),
+                    Scores(),
+                    Scores(),
+                    Scores(),
+                    Scores(),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],     
-     ),
-    ),
-    ],
+      ),
     );
   }
 }
 
-class InfoLigne extends StatelessWidget {
-  final String libelle;
-  final String valeur;
-  final bool showDivider; // Le séparateur optionnel
-
-  const InfoLigne({
-    super.key,
-    required this.libelle,
-    required this.valeur,
-    this.showDivider = true, // Par défaut, on l'affiche
-  });
+class Scores extends StatelessWidget {
+  const Scores({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
+        IntrinsicHeight(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: .start,
             children: [
-              Text(
-                libelle,
-                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              Expanded(
+                flex: 44,
+                child: _Nutriscore(nutriscore: ProductNutriScore.B),
               ),
-              Text(
-                valeur,
-                style: const TextStyle(fontSize: 16, color: Colors.grey),
+              VerticalDivider(),
+              Expanded(
+                flex: 56,
+                child: _NovaGroup(novaScore: ProductNovaScore.group4),
               ),
             ],
           ),
         ),
-        if (showDivider)
-          const Divider(height: 1, thickness: 1, color: Color(0xFFEEEEEE)),
+        Divider(),
+        _GreenScore(greenScore: ProductGreenScore.A),
       ],
     );
+  }
+}
+
+class _Nutriscore extends StatelessWidget {
+  const _Nutriscore({required this.nutriscore});
+
+  final ProductNutriScore nutriscore;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          AppLocalizations.of(context)!.nutriscore,
+          style: context.theme.title3,
+        ),
+        const SizedBox(height: 5.0),
+        Image.asset(_findAssetName(), height: 42.0),
+      ],
+    );
+  }
+
+  String _findAssetName() {
+    return switch (nutriscore) {
+      ProductNutriScore.A => 'res/drawables/nutriscore_a.png',
+      ProductNutriScore.B => 'res/drawables/nutriscore_b.png',
+      ProductNutriScore.C => 'res/drawables/nutriscore_c.png',
+      ProductNutriScore.D => 'res/drawables/nutriscore_d.png',
+      ProductNutriScore.E => 'res/drawables/nutriscore_e.png',
+      ProductNutriScore.unknown => 'TODO',
+    };
+  }
+}
+
+class _NovaGroup extends StatelessWidget {
+  const _NovaGroup({required this.novaScore});
+
+  final ProductNovaScore novaScore;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          AppLocalizations.of(context)!.nova_group,
+          style: context.theme.title3,
+        ),
+        const SizedBox(height: 5.0),
+        Text(_findLabel(), style: const TextStyle(color: AppColors.grey2)),
+      ],
+    );
+  }
+
+  String _findLabel() {
+    return switch (novaScore) {
+      ProductNovaScore.group1 =>
+        'Aliments non transformés ou transformés minimalement',
+      ProductNovaScore.group2 => 'Ingrédients culinaires transformés',
+      ProductNovaScore.group3 => 'Aliments transformés',
+      ProductNovaScore.group4 =>
+        'Produits alimentaires et boissons ultra-transformés',
+      ProductNovaScore.unknown => 'Score non calculé',
+    };
+  }
+}
+
+class _GreenScore extends StatelessWidget {
+  const _GreenScore({required this.greenScore});
+
+  final ProductGreenScore greenScore;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          AppLocalizations.of(context)!.greenscore,
+          style: context.theme.title3,
+        ),
+        const SizedBox(height: 5.0),
+        Row(
+          children: <Widget>[
+            Icon(_findIcon(), color: _findIconColor()),
+            const SizedBox(width: 10.0),
+            Expanded(
+              child: Text(
+                _findLabel(),
+                style: const TextStyle(color: AppColors.grey2),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  IconData _findIcon() {
+    return switch (greenScore) {
+      ProductGreenScore.APlus => AppIcons.ecoscore_a_plus,
+      ProductGreenScore.A => AppIcons.ecoscore_a,
+      ProductGreenScore.B => AppIcons.ecoscore_b,
+      ProductGreenScore.C => AppIcons.ecoscore_c,
+      ProductGreenScore.D => AppIcons.ecoscore_d,
+      ProductGreenScore.E => AppIcons.ecoscore_e,
+      ProductGreenScore.F => AppIcons.ecoscore_f,
+      ProductGreenScore.unknown => AppIcons.ecoscore_e,
+    };
+  }
+
+  Color _findIconColor() {
+    return switch (greenScore) {
+      ProductGreenScore.APlus => AppColors.greenScoreAPlus,
+      ProductGreenScore.A => AppColors.greenScoreA,
+      ProductGreenScore.B => AppColors.greenScoreB,
+      ProductGreenScore.C => AppColors.greenScoreC,
+      ProductGreenScore.D => AppColors.greenScoreD,
+      ProductGreenScore.E => AppColors.greenScoreE,
+      ProductGreenScore.F => AppColors.greenScoreF,
+      ProductGreenScore.unknown => Colors.transparent,
+    };
+  }
+
+  String _findLabel() {
+    return switch (greenScore) {
+      ProductGreenScore.APlus => 'Très faible impact environnemental',
+      ProductGreenScore.A => 'Très faible impact environnemental',
+      ProductGreenScore.B => 'Faible impact environnemental',
+      ProductGreenScore.C => "Impact modéré sur l'environnement",
+      ProductGreenScore.D => 'Impact environnemental élevé',
+      ProductGreenScore.E => 'Impact environnemental très élevé',
+      ProductGreenScore.F => 'Impact environnemental très élevé',
+      ProductGreenScore.unknown => 'Score non calculé',
+    };
   }
 }
